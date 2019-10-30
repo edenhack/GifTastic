f52b966a085b6fe34f1b4b707530acaa70b2dcca
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-10-30 08:32:50 -0500

    Added renderbutton function, added basic array, added api key and queryURL

---
+++ assets/javascript/program.js
@@ -0,0 +1,15 @@
+const apiKey = "twTHuGw6oDIHbPETsVzkb0cXOJcK4Eqd";
+let search = $(".carButton").val();
+const queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=1&rating=G`;
+const cars = ["BMW e46 M3","Nissan 370z","Koenisegg Agera R","Porsche 911 GT2"];
+
+function renderButtons(){
+    $("#buttons-view").empty();
+    for (let i = 0; i < movies.length; i++){
+        const a = $("<button>");
+        a.addClass("car");
+        a.attr("data-name",cars[i]);
+        a.text(movies[i]);
+        $("button-view").append(a);
+    }
+}
