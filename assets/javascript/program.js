5be7eadde0b1c4b593565a1b360d9e2b4dcc5c9f
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-10-30 11:08:32 -0500

    added ajax call, not working currently

--- assets/javascript/program.js
+++ assets/javascript/program.js
@@ -1,22 +1,36 @@
-const apiKey = "twTHuGw6oDIHbPETsVzkb0cXOJcK4Eqd";
-let search = $(".carButton").val();
-const queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=1&rating=G`;
-const cars = ["BMW e46 M3","Nissan 370z","Koenisegg Agera R","Porsche 911 GT2"];
+const cars = ["BMW e46 M3", "Nissan 370z", "Koenisegg Agera R", "Porsche 911 GT2"];
 
-function renderButtons(){
+function renderButtons() {
     $("#buttons-view").empty();
-    for (let i = 0; i < movies.length; i++){
+    for (let i = 0; i < cars.length; i++) {
         const a = $("<button>");
         a.addClass("car");
-        a.attr("data-name",cars[i]);
-        a.text(movies[i]);
-        $("button-view").append(a);
+        a.attr("data-car", cars[i]);
+        a.text(cars[i]);
+        $("#buttons-view").append(a);
     }
 }
 
-$("add-car").on("click",function(event){
+$("#add-car").on("click", function (event) {
     event.preventDefault();
     const car = $("#car-input").val().trim();
     cars.push(car);
     renderButtons();
-})
+});
+
+$(".car").on("click", function () {
+
+    const apiKey = "twTHuGw6oDIHbPETsVzkb0cXOJcK4Eqd";
+    const carVal = $(this).attr("data-car");
+    const queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${carVal}&limit=1&rating=G`;
+
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function (response) {
+            console.log(response);
+        });
+});
+
+renderButtons();
