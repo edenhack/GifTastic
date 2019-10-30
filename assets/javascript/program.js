76de3b54bc8d039cf6e958d4693955f5ae9616b1
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-10-30 17:57:00 -0500

    added bootstrap, converted standard buttons to bootstrap buttons and adjusted jquery selector for ajax call

--- assets/javascript/program.js
+++ assets/javascript/program.js
@@ -1,16 +1,21 @@
 const cars = ["BMW e46 M3", "Nissan 370z", "Koenisegg Agera R", "Porsche 911 GT2"];
 
+
+//Renders the buttons to screen.
 function renderButtons() {
     $("#buttons-view").empty();
     for (let i = 0; i < cars.length; i++) {
         const a = $("<button>");
-        a.addClass("car");
+        a.attr("type", "button");
+        a.addClass("btn btn-success m-1");
         a.attr("data-car", cars[i]);
         a.text(cars[i]);
         $("#buttons-view").append(a);
     }
 }
 
+
+// Creates new buttons.
 $("#add-car").on("click", function (event) {
     event.preventDefault();
     const car = $("#car-input").val().trim();
@@ -18,7 +23,9 @@ $("#add-car").on("click", function (event) {
     renderButtons();
 });
 
-$(".car").on("click", function () {
+
+// FIXME on click button giphy search api call.
+$(".successs").on("click", function () {
 
     const apiKey = "twTHuGw6oDIHbPETsVzkb0cXOJcK4Eqd";
     const carVal = $(this).attr("data-car");
