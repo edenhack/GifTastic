2bc81ea5f85867b028480656744901420bd6a350
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-10-30 22:37:47 -0500

    fixed ajax call for buttons, added animate reaction on click for images

--- assets/javascript/program.js
+++ assets/javascript/program.js
@@ -25,7 +25,7 @@ $("#add-car").on("click", function (event) {
 
 
 // FIXME on click button giphy search api call.
-$(".successs").on("click", function () {
+function displayCarInfo() {
 
     const apiKey = "twTHuGw6oDIHbPETsVzkb0cXOJcK4Eqd";
     const carVal = $(this).attr("data-car");
@@ -36,8 +36,40 @@ $(".successs").on("click", function () {
             method: "GET"
         })
         .then(function (response) {
-            console.log(response);
+            // console.log(response);
+            const carDiv = $("<div class='carDiv'>");
+            const results = response.data;
+            for (let i = 0; i < results.length; i++) {
+                $(this).addClass("gif");
+                const imgURL = results[i].images.fixed_height_small_still.url;
+                const image = $("<img>").attr("src", imgURL);
+                $(this).attr("data-state", "still");
+                carDiv.append(image);
+                $("#Image-View").prepend(carDiv);
+
+                //consts for still and animate gif links
+                const still = results[i].images.fixed_height_small_still.url;
+                const animate = results[i].images.fixed_height_small.url;
+
+                // consts for the data attributes of the gifs
+                const animatedGif = image.attr("data-animated", animate);
+                const stillGif = image.attr("data-still", still);
+
+                $(".gif").on("click", function () {
+                    const state = $(this).attr("data-state");
+                    if (state === "still") {
+                        $(this).attr("src", animate);
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        $(this).attr("src", still);
+                        $(this).attr("data-state", "still");
+                    }
+                });
+
+
+            }
         });
-});
+};
+$(document).on("click", ".btn-success", displayCarInfo);
 
 renderButtons();
